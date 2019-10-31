0853dcf0af50de6cc1959259428b98eab9de3b61
Andy Laughlin <andy.laughlin@airts.co.uk>
2019-10-31 09:03:35 +0000

    just added editProfile controller and related page

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -1,45 +1,10 @@
 'use strict';
 
 eventsApp.controller('EventController', 
-    function EventController($scope) {
+    function EventController($scope, eventData) {
         
         $scope.sortorder = 'name';
-        $scope.event = {
-            name: 'Angular Boot Camp',
-            date: '1/1/2013',
-            time: '10:30 am',
-            location: {
-                address: '18 Murieston Drive',
-                city: 'Livingston',
-                area: 'West Lothian'
-            },
-            sessions: [
-                {
-                    name: 'Directives Masterclass',
-                    creatorName: 'Bob Smith',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Scopes for Fun and Profit',
-                    creatorName: 'Andy Laughlin',
-                    duration: 2,
-                    level: 'Introductory',
-                    abstract: 'This sessions will take a closer look at scopes, learn what they do etc',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Well-behaved Controllers',
-                    creatorName: 'Ryan Christie',
-                    duration: 4,
-                    level: 'Intermediate',
-                    abstract: 'Learn how controllers work and how to use them correctly!',
-                    upVoteCount: 0
-                }
-            ]
-        };
+        $scope.event = eventData.event;
         
         $scope.upVoteSession = function(session){
             session.upVoteCount++;
